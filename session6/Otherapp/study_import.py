caf17e4d27d0d8ee7bf7c5c0e4a1cb7b90459b91
Manish Zope <manishz@opera.com>
2015-03-13 10:38:09 +0530

    Comments removed

--- session6/Otherapp/study_import.py
+++ session6/Otherapp/study_import.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python
 import sys
 import os
+'''
 print __file__ 
 print os.path.realpath(__file__) 
 print os.path.dirname(os.path.realpath(__file__))
 print os.path.dirname(os.path.realpath(__file__))+'/../'
-
+'''
 #sys.path.insert(0, '/mnt/python-sessions/session6')
 sys.path.insert(0, os.path.dirname(os.path.realpath(__file__))+'/../')
 
 
-'''
+
 import Myapp.util.mymath
-from Myapp.util.mymath import Session5, Session5_sub
-from Myapp.util.mymath import Session5 as main, Session5_sub as sub
 
 print 'We are looking at 1st method of import \n'
 print Myapp.util.mymath.Session5.add_it_class()
@@ -27,20 +26,17 @@ print "CLASS NAME FROM OBJECT :: "+sess5obj.__class__.__name__
 print Myapp.util.mymath.Session5_sub.add_it_class()
 
 
- ===================================================================================================		
-print '\n\n We are looking at 2nd  method of import \n'
-print Session5.add_it_class()
+'''  =================================================================================================== '''
 
-print Session5.add_it_statics()
-
-sess5obj = Session5(5,10)
-print sess5obj.add_it_object()
-print "CLASS NAME FROM OBJECT :: "+sess5obj.__class__.__name__
-
+from Myapp.util.mymath import Session5_sub
+print "Example of selective import"
 print Session5_sub.add_it_class()
 
 
-===================================================================================================	
+''' ===================================================================================================	 '''
+
+from Myapp.util.mymath import Session5 as main, Session5_sub as sub
+
 print '\n\nWe are looking at 3rd method of import \n'
 print main.add_it_class()
 
@@ -51,4 +47,3 @@ print sess5obj.add_it_object()
 print "CLASS NAME FROM OBJECT :: "+sess5obj.__class__.__name__
 
 print sub.add_it_class()
-'''
